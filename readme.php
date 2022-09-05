69feea6c8b65e49048faf628b4edd40ecbebe6e4
Emmanuel-Mmanda <112853016+EmmanuelMmanda@users.noreply.github.com>
2022-09-05 12:29:39 +0300

    Create readme.php

---
+++ readme.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Remarks</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+
+        .wrapper {
+
+            font-size: 20px;
+            display: flex;
+            justify-content: center;
+            border-radius: 20px;
+            margin: 20px;
+
+        }
+
+        .content {
+            justify-self: center;
+            width: 700px;
+            background-color: #eee;
+            padding: 20px;
+
+        }
+    </style>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="content">
+            <h1>Project Remarks:</h1>
+            <p>This sytem was created to as a first prototype -system for
+                an Maternity Information system that could help maternals to make appointments in their Health facility ,
+              
+            </p>
+          <p> The System has been intergrated with an SMS API that allow medical Officers to send Approved Appointments Notifications through mobile SMS .
+            <h3>Features of Herbal Information System</h3>
+            <ul>
+                <li>Maternals Appointments</li>
+                <li>Mobile SMS Notifiications</li>
+                <li>Medical Officers Dashboard</li>
+                <li>Maternals Dashboard</li>
+                <li>PDF Reports</li>
+                
+            </ul>
+            <div>
+                <div style="display: flex;">
+                    <div>
+                        <img src="upload/user.png" alt="usericon" width="110px" height="110px">
+                    </div>
+                    <div>
+                        <p> Developed By: Emmanuel Mmanda </p>
+                        <p>Ardhi University Student @ 2022 | Tanzania</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+
+    </script>
+</body>
+</html>
